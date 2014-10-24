2b9a69d9b7671eec80611fa09ec0f85f8e32421d
sscruz <s.scruz.312@gmail.com>
2014-10-24 20:37:35 +0200

    setup.sh supports now sc5 and sc6

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
-source /nfs/fanae/root_releases/root.5.34.13.slc6/bin/thisroot.sh
-source /nfs/fanae/PAF_releases/PAF_devel/PAF_setup.sh 
-source /opt/PoD/PoD_env.sh 
+#!/bin/bash
+
+ker=$(uname -r) 
+kersc5=2.6.18-371.8.1.el5
+
+
+if [ "$ker" == "$kersc5" ]; then
+    echo Setting up variables for SC5
+    source /nfs/fanae/root_releases/root.5.34.13.slc5/bin/thisroot.sh
+    source /nfs/fanae/PAF_releases/PAF_devel/PAF_setup.sh 
+    source /opt/PoD/PoD_env.sh 
+else
+    echo Setting up variables for SC6
+    source /nfs/fanae/root_releases/root.5.34.13.slc6/bin/thisroot.sh
+    source /nfs/fanae/PAF_releases/PAF_devel/PAF_setup.sh 
+    source /opt/PoD/PoD_env.sh 
+fi
